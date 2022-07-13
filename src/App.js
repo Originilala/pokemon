cfc3b6c11dc8df1bc20127c18059121c222aeb7b
originilala <jonathan.dewsaran@gmail.com>
2022-07-13 16:00:31 +0200

    hand in assignment anyway

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+import Pokemon from "./components/Pokemon.js";
 
 function App() {
     const [resultData, setResultData] = useState(null);
+    const [query, setQuery] = useState('pokemon');
     const endpoint = 'https://pokeapi.co/api/v2/';
-    const query = 'pokemon/jigglypuff';
+    // const query = 'pokemon/jigglypuff';
+    // const query2 = 'pokemon/ditto';
+    // const query3 = 'pokemon';
 
+    }
+    useEffect(()=>{
     async function fetchData(){
         try{
             const { data } = await axios.get(`${endpoint}${query}`);
@@ -17,6 +23,7 @@ function App() {
     }
 
     fetchData();
+    },[query]);
 
     // useEffect(()=>{
     //     fetchData()
@@ -25,20 +32,16 @@ function App() {
   return (
     <div>
       Begin hier met de opdracht!
-        <article>
-            {resultData && <img
-                src={resultData.sprites.front_shiny}
-                alt={resultData.forms[0].name}
-            />}
-            {resultData && <h2>{resultData.forms[0].name}</h2>}
-            {resultData && <ul>
-                {resultData.abilities.map((entry) => {
-                    return(<li>{entry.ability.name}</li>)
-                })}
-            </ul>}
-            {resultData && <p>{resultData.weight}</p>}
-            {resultData && <p>{resultData.moves.length}</p>}
-        </article>
+        <button onClick={setQuery(resultData.next.slice('v2/',))} {resultData.next && disabled}>Vorige</button>
+        <button onClick={setQuery(resultData.previous.slice('v2/',))} {resultData.previous && disabled}>Volgende</button>
+
+        {resultData && <ul>
+            {resultData.results.map((entry) => {
+                setQuery(entry.name);
+                return(<Pokemon
+                data={entry}
+            />)})}
+        </ul>}
     </div>
   );
 }
